f60c13931121028ab50ac83955cb5142a709d029
satoyuki <yuki01377@gmail.com>
2020-08-03 23:39:55 +0900

    フロントエンドの実装完了

---
+++ src/router/guards.js
@@ -0,0 +1,20 @@
+import store from '../store'
+
+export const authorizeToken = (to, from, next) => {
+  console.log(to)
+  if (to.matched.some(record => record.meta.requiresAuth)) {
+    // マッチしたルートにおいて、メタフィールドに`requireszsuth`が付与されている場合は
+    // ログインした際に付与される認証トークンがあるかどうかチェックする
+    // 注意:
+    // このアプリケーションでは簡略化のため`auth.token`があるかどうかのみで
+    // ログイン済みであるかどうかチェックしているが、
+    // 本来ならば付与された認証トークンをバックエンドのAPI経由などで検証すべき
+    if (!store.state.auth || !store.state.auth.token) {
+      next({path: '/login'})
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
+}
